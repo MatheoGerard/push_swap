333dc423f8868a86c57097128fc2a978ba573b01
Noa Lovius <nlovius@fu-r3-p8.s19.be>
2026-05-15 19:28:05 +0200

    safe

--- quick_4.c
+++ quick_4.c
@@ -6,13 +6,19 @@
 /*   By: nlovius <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 13:44:16 by nlovius           #+#    #+#             */
-/*   Updated: 2026/04/28 13:40:02 by nlovius          ###   ########.fr       */
+/*   Updated: 2026/05/15 15:06:29 by nlovius          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
-int	partitions(t_stacks *ab, char in_stack, int low, int high)
+int	find_index(t_stack *ab, int pivot_index)
+{
+	return (pivot_index);
+}
+
+int	partitions(t_stack *ab, char in_stack, int low, int high, t_op_count *values, int is_bench)
 {
 	int	p_val;
 	int	pi;
@@ -27,14 +33,15 @@ int	partitions(t_stacks *ab, char in_stack, int low, int high)
 	{
 		if (in_stack == 'a' && stack->nbrs[0] < p_val ||
 				in_stack == 'b' && stack->nbrs[0] > p_val)
-			pushfrom(&ab[0], &ab[1], in_stack);
+			pushfrom(ab, in_stack, is_bench, values);
 		else
-			rotate(&ab[0], &ab[1], in_stack);
+			rotate(ab, in_stack, values, is_bench);
 	}
 	stack = &ab[0];
 	while (i < stack->current_len)
 	{
 		stack->index[i] = i + (&ab[1])->current_len;
+		printf("%d --> %d\n", stack->index[i], stack->nbrs[i]);
 		i++;
 	}
 	stack = &ab[1];
@@ -42,7 +49,8 @@ int	partitions(t_stacks *ab, char in_stack, int low, int high)
 	while (i < stack->current_len)
 	{
 		stack->index[i] = stack->current_len - i;
+		printf("%d --> %d\n", stack->index[i], stack->nbrs[i]);
 		i++;
 	}
-	return (find_index(a, b, p_val));
+	return (find_index(ab, p_val));
 }
